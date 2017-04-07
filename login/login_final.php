a7e4107d3f8fcdcf7c349e4792146cd8d2587565
yniu3 <helenaniu2006@gmail.com>
2017-04-07 18:11:56 -0400

    Create login_final.php

---
+++ login/login_final.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html lang="en"><head>
+  
+  <link rel="stylesheet" type="text/css" href="style.css">
+
+  
+  <meta charset="UTF-8">
+
+  
+  <meta http-equiv="X-UA-Compatible" content="EI=edge">
+
+  
+  <meta name="viewpoint" content="width = device-width, initial-scare =1">
+  <title>Login</title>
+
+  
+  
+  <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+
+  
+  <style>
+.jumbotron{
+	background-color:#2E2D88;
+	color:white;
+}
+/*Adds borders for tabs*/
+.tab-content{
+	border-left: 1px solid #add;
+	border-right: 1px solid #add;
+	border-bottom: 1px solid #add;
+	padding: 10px;
+}
+
+.nav-tabs{
+	margin-bottom: 0;
+}
+
+#frm{
+	border: solid gray 1px;
+	width: 55%;
+	border-radius: 5px;
+	margin: 100px auto;
+	background: white;
+	padding: 50px;
+}
+
+#btn{
+	color:#fff;
+	background: #337ab7;
+	padding: 5px;
+	margin-left: 69%;
+}
+#user{
+	color:black;
+}
+#pass{
+	color:black;
+}
+  </style>
+</head><body>
+<div class="container">
+<div class="page-header">
+<h1>Don't Queue, You</h1>
+</div>
+<div class="jumbotron">
+<p>Your Venue Here</p>
+<div>
+<form action="process.php" method="post">
+  <p> <label>Username:</label> &nbsp;&nbsp; <input id="user" name="user" type="text"> </p>
+
+  <p> <label>Password:</label>&nbsp; &nbsp;&nbsp; <input id="pass" name="pass" type="password"> </p>
+  <p> <label>Contact Info:</label> <input id="user" name="user" type="text"> </p>
+  <p> <label>Party Size:</label>&nbsp;&nbsp; &nbsp; <input id="user" name="user" type="text"> </p>
+  <p> <label>Notes:</label>&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;
+&nbsp;&nbsp; &nbsp; <input id="user" name="user" type="text"> </p>
+  <p> <input class="btn btn-success" value="Submit" type="submit"> <a href="Register.php"><button type="submit" class="btn btn-link">Register</button></a>
+  </p>
+</form>
+</div>
+</div>
+</div>
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script><!-- Use downloaded version of Bootstrap -->
+
+<script src="js/bootstrap.min.js"></script>
+</body></html>
